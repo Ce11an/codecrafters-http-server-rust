11852a77cb55444fc2ddc138bc59001dcd7cea24
Cellan Hall <hallcellan@gmail.com>
2024-07-03 20:09:19 +0100

    Respond with 200

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -7,8 +7,13 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                let response = "HTTP/1.1 200 OK\r\n\r\n";
+                _stream
+                    .write_all(response.as_bytes())
+                    .expect("Failed to write response");
+                // handle_connection(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
